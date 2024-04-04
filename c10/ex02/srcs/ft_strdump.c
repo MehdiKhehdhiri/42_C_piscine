211b68fd24b60ce17a1b7d763d15644e21203e91
Mehdi Khedhiri <xenm3hdi@hotmail.com>
2024-04-04 21:18:38 +0200

    Create ft_strdump.c

---
+++ c10/ex02/srcs/ft_strdump.c
@@ -0,0 +1,27 @@
+#include "../includes/ft.h"
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	while (src[i])
+		i++;
+	dest = malloc(sizeof(src) * (i + 1));
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	if (dest == NULL)
+		return (0);
+	else
+	{
+		while (src[i])
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+		return (dest);
+	}
+}
