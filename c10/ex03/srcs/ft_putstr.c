26e421878c1219fddfe7cbb8cb6e65165ce62fd7
Mehdi Khedhiri <xenm3hdi@hotmail.com>
2024-04-04 21:26:43 +0200

    Create ft_putstr.c

---
+++ c10/ex03/srcs/ft_putstr.c
@@ -0,0 +1,29 @@
+#include "../includes/ft.h"
+
+void	ft_putstr(char *str, int out)
+{
+	int i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		ft_putchar(*(str + i), out);
+		i++;
+	}
+}
+
+void	ft_putstr_buf(char *str, int size)
+{
+	int i;
+
+	i = -1;
+	while (++i < size)
+		ft_putchar(str[i], 1);
+}
+
+void	ft_putstr_headfile(char *filepath)
+{
+	ft_putstr("==> ", 1);
+	ft_putstr(basename(filepath), 1);
+	ft_putstr(" <==\n", 1);
+}
