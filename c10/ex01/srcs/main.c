4882b9f0309d57cf48018427ba062e6ebeab1d7a
Mehdi Khedhiri <xenm3hdi@hotmail.com>
2024-04-04 21:07:13 +0200

    Create main.c

---
+++ c10/ex01/srcs/main.c
@@ -0,0 +1,23 @@
+#include "../includes/ft.h"
+
+int		main(int argc, char **argv)
+{
+	int		i;
+
+	if (argc == 1)
+		no_args(argv);
+	else
+	{
+		i = 0;
+		while (++i < argc)
+			if (ft_file_read(argv[i]) == 0)
+			{
+				ft_putstr(basename(argv[0]), 2);
+				ft_putstr(": ", 2);
+				ft_putstr(argv[i], 2);
+				ft_putstr(": ", 2);
+				ft_putstr(strerror(errno), 2);
+				ft_putchar('\n', 2);
+			}
+	}
+}
